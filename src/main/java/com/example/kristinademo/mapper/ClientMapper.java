1af22643185dc50372fdb7bdf2304772dad01fdf
Sorokin Aleksey <ktyz55ktyz@yandex.ru>
2023-02-21 18:46:54 +0300

    удалил коменты

--- src/main/java/com/example/kristinademo/mapper/ClientMapper.java
+++ src/main/java/com/example/kristinademo/mapper/ClientMapper.java
@@ -4,13 +4,8 @@ import com.example.kristinademo.dto.ClientDto;
 import com.example.kristinademo.model.Client;
 import org.mapstruct.Mapper;
 
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper extends MapperAll<ClientDto,Client> {
-//    ClientDto mapperDTO(Client type);
-//    Client mapperType(ClientDto dto);
-//    List<ClientDto> mapperListDto(List<Client> type);
-//
-//    List<Client> mapperListType(List<ClientDto> dto);
+
 }
